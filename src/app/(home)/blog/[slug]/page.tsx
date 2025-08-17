ffc1ce00fc61808ca1d7d98a5bab00c9e2bb5782
ahgaedi <aelgaedy@gmail.com>
2025-08-17 15:05:34 +0300

    fix pageProps type

--- src/app/(home)/blog/[slug]/page.tsx
+++ src/app/(home)/blog/[slug]/page.tsx
@@ -1,54 +1,45 @@
-
 import { notFound } from "next/navigation";
-
-
 import blogData from "@/app/data/blog.json";
 import Image from "next/image";
 import Link from "next/link";
 import {ArrowLeft} from "lucide-react";
 
-export default function BlogPostPage({ params }: { params: { slug: string } }) {
-    const post = blogData.blogPosts[params.slug as keyof typeof blogData.blogPosts];
+export default async function BlogPostPage({ params }: { params: Promise<{ slug: string }> }) {
+    const { slug } = await params;
+    const post = blogData.blogPosts[slug as keyof typeof blogData.blogPosts];
 
     if (!post) return notFound();
 
     return (
         <>
-
-
             <div className="sticky top-0 w-full flex items-center gap-2 mt-6 h-14 z-10 bg-[#111111]">
-            <Link
-                href="/blog"
-                className="flex items-center gap-2 text-neutral-400 hover:text-white transition-colors"
-            >
-                <ArrowLeft size={16} />
-                Back to Blog
-            </Link>
-        </div>
-
+                <Link
+                    href="/blog"
+                    className="flex items-center gap-2 text-neutral-400 hover:text-white transition-colors"
+                >
+                    <ArrowLeft size={16} />
+                    Back to Blog
+                </Link>
+            </div>
 
             <article className=" py-2 flex flex-col gap-4 ">
                 <h1 className="text-3xl font-bold ">{post.title}</h1>
 
-
-
                 <p className="text-sm text-neutral-400 sm:flex-row flex-col items-start sm:items-center flex gap-2">
-                        <span >
-                          By {post.author}
-                        </span>
+                    <span >
+                        By {post.author}
+                    </span>
                     <span className="sm:flex hidden">
-                          &bull;
-                        </span>
+                        &bull;
+                    </span>
                     <span>
-                          Published on {new Date(post.date).toLocaleDateString()}
-                        </span>
-
+                        Published on {new Date(post.date).toLocaleDateString()}
+                    </span>
                 </p>
                 <hr className="opacity-20" />
                 {/*<Image src={post.img} alt={post.title} className="rounded-lg mb-6" />*/}
                 <p className="text-lg leading-7 whitespace-pre-line">{post.content}</p>
             </article>
         </>
-
     );
 }
