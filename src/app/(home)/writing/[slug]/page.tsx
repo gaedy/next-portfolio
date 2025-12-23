6affc7ca40a0963d18b6630c918e3eb4d1755a55
ahgaedi <aelgaedy@gmail.com>
2025-12-23 15:27:50 +0200

    feat: add reading time estimation to blog posts and update UI for better readability

--- src/app/(home)/writing/[slug]/page.tsx
+++ src/app/(home)/writing/[slug]/page.tsx
@@ -3,6 +3,8 @@ import blogData from "@/app/data/blog.json";
 
 import ReactMarkdown from "react-markdown";
 import { Metadata } from "next";
+import readingTime from "reading-time";
+import { Clock, Timer } from "lucide-react";
 
 type Props = {
   params: Promise<{ slug: string }>; // Updated type to reflect Promise
@@ -36,16 +38,26 @@ export default async function BlogPostPage({
     year: "numeric",
   });
 
+  const stats = readingTime(post.content);
+  const minutes = Math.ceil(stats.minutes);
+
   return (
     <article className="py-2 flex flex-col gap-4">
       <header>
         <h1 className="text-3xl font-bold">{post.title}</h1>
       </header>
 
-      <div className="text-sm font-outfit text-muted-foreground sm:flex-row flex-col items-start sm:items-center flex gap-2">
+      <div className="text-sm text-muted-foreground flex-row items-center flex gap-2 flex-wrap">
         {/* <p>By {post.author}</p>
         <span className="sm:flex hidden">&bull;</span> */}
-        <p>Published on {currentDate}</p>
+
+        <div className="flex items-center  gap-1">
+          {/* <Timer size={14} /> */}
+          <span >{minutes} min read</span >
+        </div>
+        <span className="">&bull;</span>
+
+        <p>{currentDate}</p>
       </div>
 
       <hr className="bg-border " />
